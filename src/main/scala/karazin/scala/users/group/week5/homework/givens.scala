e5de607c79f82f86efc011a9a69f2716cdbf73c7
Qdesker <liza3450087@gmail.com>
2022-06-28 15:00:03 +0300

    Week-5 - Tasks and tests

--- src/main/scala/karazin/scala/users/group/week5/homework/givens.scala
+++ src/main/scala/karazin/scala/users/group/week5/homework/givens.scala
@@ -1,19 +1,33 @@
 package karazin.scala.users.group.week5.homework
 
 object givens:
-  
+
   /* 
     The trait is used for converting instances to a json string representation
     Provide a type parameter(s) for the trait and the method 
     and argument(s) and a result type
-  */ 
-  
-  trait DummyType
-  
-  trait JsonStringEncoder:
-    def encode: DummyType
+  */
 
-  /* 
-    Make sure that integers, booleans, strings and lists 
-    are convertable to a json string representation 
-  */ 
+  trait JsonStringEncoder[T]:
+    def encode(src: T): String
+
+  given IntEncoder: JsonStringEncoder[Int] with
+    def encode(src: Int): String = src.toString
+
+  given BoolEncoder: JsonStringEncoder[Boolean] with
+    def encode(src: Boolean): String = src.toString
+
+  given StringEncoder: JsonStringEncoder[String] with
+    def encode(src: String): String = "\"" + src + "\""
+
+  given ListEncoder[T](using encoder: => JsonStringEncoder[T]): JsonStringEncoder[List[T]] with
+    def encode(src: List[T]): String = "[" + src.foldLeft(List[String]()) {(acc, src) => acc :+ encoder.encode(src)}.mkString(", ") + "]"
+
+  object JsonStringEncoder:
+    def apply[V](using encoder: => JsonStringEncoder[V]): JsonStringEncoder[V] =
+      encoder
+
+/*
+  Make sure that integers, booleans, strings and lists
+  are convertable to a json string representation
+*/
--- src/test/scala/karazin/scala/users/group/week5/homework/GivensSuite.scala
+++ src/test/scala/karazin/scala/users/group/week5/homework/GivensSuite.scala
@@ -2,6 +2,8 @@ package karazin.scala.users.group.week5.homework
 
 import scala.concurrent.Future
 
+import karazin.scala.users.group.week5.homework.givens.{given, _}
+
 /*
   Write test for all programs in karazin.scala.users.group.week4.homework.givens
 
@@ -17,12 +19,82 @@ import scala.concurrent.Future
     • https://scalameta.org/munit/docs/tests.html
     • https://scalameta.org/munit/docs/assertions.html
     • https://scalameta.org/munit/docs/assertions.html#compileerrors
-    
+
   NB: Do not use sync, this homework does not belong async stuff
-    
+
  */
 class GivensSuite extends munit.FunSuite:
-  
-  test("successful test example") {
-    assertEquals(42, 42)
+
+  test("Integer") {
+    assertEquals(
+      JsonStringEncoder[Int]
+        .encode(200), "200"
+    )
+  }
+
+  test("Integer negative") {
+    assertEquals(
+      JsonStringEncoder[Int]
+        .encode(-7), "-7"
+    )
+  }
+
+  test("Boolean false") {
+    assertEquals(
+      JsonStringEncoder[Boolean]
+        .encode(false), "false"
+    )
+  }
+
+  test("Boolean true") {
+    assertEquals(
+      JsonStringEncoder[Boolean]
+        .encode(true), "true"
+    )
+  }
+
+  test("String") {
+    assertEquals(
+      JsonStringEncoder[String]
+        .encode("Contra Spem Spero"), "\"Contra Spem Spero\""
+    )
+  }
+
+  test("Empty") {
+    assertEquals(
+      JsonStringEncoder[String]
+        .encode(""), "\"\""
+    )
+  }
+
+  test("List String") {
+    assertEquals(
+      JsonStringEncoder[List[String]]
+        .encode("Contra" :: "Spem" :: "Spero" :: Nil), "[\"Contra\", \"Spem\", \"Spero\"]"
+    )
+  }
+
+  test("List Booleans") {
+    assertEquals(
+      JsonStringEncoder[List[Boolean]]
+        .encode(true :: false :: true :: Nil), "[true, false, true]"
+    )
+  }
+
+  test("List Integers") {
+    assertEquals(
+      JsonStringEncoder[List[Int]]
+        .encode(90 :: -119 :: 0 :: Nil), "[1, -2, 0]"
+    )
+  }
+
+  test("List Empty") {
+    assertEquals(
+      JsonStringEncoder[List[Int]]
+        .encode(Nil), "[]"
+    )
+  }
+
+  test("Map fails") {
+    compileErrors("JsonStringEncoder[Map[String, Int]].encode(Map(\"x\" -> 100, \"y\" -> -2, \"z\" -> 0))")
   }
